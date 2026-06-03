ef1e3e0ba2b4bc1e012b4293ef47d5cf6eec9308
Satyam Sinha <satyamsinha920@gmail.com>
2026-06-04 00:49:57 +0530

    add the service registary again

---
+++ backend/service-registary/src/test/java/com/satyam/service_registary/ServiceRegistaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyam.service_registary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
